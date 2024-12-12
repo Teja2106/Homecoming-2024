a1e6036591511df992043cca85b22f22dcb4ff35
Teja2106 <teja182106@gmail.com>
2024-12-12 23:49:39 +0530

    Initial Commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,27 @@
+import Navbar from './Navbar';
+import Header from './Header'
+import Events from "./Events"
+import Gallery from './Gallery'
+import Team from "./Team";
+import Gallerymain from './Gallerymain';
+import Video from './Video';
+import Merch from './Merch';
+import Guidelines from './Guidelines';
+import Footer from './Footer';
+
+export default function Main() {
+  return (
+    <div>
+      <Navbar />
+      <Video />
+      <Header />
+      <Events />
+      <Gallerymain />
+      <Gallery />
+      < Merch />
+      <Guidelines />
+      <Team />
+      <Footer/>
+    </div>
+  )
+}
